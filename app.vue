dd5031a89d708bbf3fcb44b0857a876282ec2f51
JD-Orkest <julien.deprez@orkest.tech>
2026-04-13 14:52:39 +0200

    feat: ajout de la gestion des métadonnées SEO avec script JSON-LD pour le site Nursy

--- app.vue
+++ app.vue
@@ -4,6 +4,25 @@ const { locale } = useI18n()
 useHead(computed(() => ({
   htmlAttrs: { lang: locale.value },
 })))
+
+// Nom de marque officiel pour les SERP Google
+useSeoMeta({
+  ogSiteName: 'Nursy',
+})
+
+useHead({
+  script: [
+    {
+      type: 'application/ld+json',
+      innerHTML: JSON.stringify({
+        '@context': 'https://schema.org',
+        '@type': 'WebSite',
+        name: 'Nursy',
+        url: 'https://nursy.be/',
+      }),
+    },
+  ],
+})
 </script>
 
 <template>
